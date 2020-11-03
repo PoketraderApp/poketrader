4586f3112dee5679fab94e6f041ec1421f53c47d
Gabriel <gabrieldr@id.uff.br>
2020-11-03 01:24:33 -0300

    Cadastro layout
    Layout das telas inicial e de cadastro do usuário desenvolvidas através do storyboard. Somente parte visual das telas.

---
+++ Poketrader/Poketrader/Views/CadastroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CadastroViewController.swift
+//  Poketrader
+//
+//  Created by Gabriel Duarte on 03/11/20.
+//
+
+import UIKit
+
+class CadastroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
