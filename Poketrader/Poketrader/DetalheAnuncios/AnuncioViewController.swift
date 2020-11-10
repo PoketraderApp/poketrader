7b8e55e9277ddd81590eb796e8ee6f78f53466b5
paulo vieira <paulorsv01@outlook.com>
2020-11-10 17:30:20 -0300

    criação da tela de detalhes do anúncio

---
+++ Poketrader/Poketrader/DetalheAnuncios/AnuncioViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AnuncioViewController.swift
+//  Poketrader
+//
+//  Created by Paulo Vieira on 10/11/20.
+//
+
+import UIKit
+
+class AnuncioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
