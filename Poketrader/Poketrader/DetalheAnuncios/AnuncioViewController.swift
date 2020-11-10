cc4eb9bf490c149a61f92d807ee97ec98cf559f6
paulo vieira <paulorsv01@outlook.com>
2020-11-10 18:01:25 -0300

    feita a ligação dos objetos da view na viewcontroller

--- Poketrader/Poketrader/DetalheAnuncios/AnuncioViewController.swift
+++ Poketrader/Poketrader/DetalheAnuncios/AnuncioViewController.swift
@@ -8,22 +8,46 @@
 import UIKit
 
 class AnuncioViewController: UIViewController {
-
+    
+    // Imagens
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    // Campo do usuário
+    @IBOutlet weak var usuarioLabel: UILabel!
+    @IBOutlet weak var usuarioValueLabel: UILabel!
+    
+    // Campo do telefone
+    @IBOutlet weak var telefoneLabel: UILabel!
+    @IBOutlet weak var telefoneValueLabel: UILabel!
+    
+    // Campo do jogo
+    @IBOutlet weak var jogoLabel: UILabel!
+    @IBOutlet weak var jogoValueLabel: UILabel!
+    
+    // Campo de email
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailValueLabel: UILabel!
+    
+    // Campo de observações
+    @IBOutlet weak var observacoesLabel: UILabel!
+    @IBOutlet weak var observacoesValueLabel: UILabel!
+    
+    // Campo de atributos
+    @IBOutlet weak var atributosLabel: UILabel!
+    @IBOutlet weak var atributosStackView: UIStackView!
+    
+    @IBOutlet weak var hpValueLabel: UILabel!
+    @IBOutlet weak var defesaValueLabel: UILabel!
+    @IBOutlet weak var ataqueValueLabel: UILabel!
+    @IBOutlet weak var velocidadeValueLabel: UILabel!
+    @IBOutlet weak var ataqueSPValueLabel: UILabel!
+    @IBOutlet weak var defesaSPValueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
