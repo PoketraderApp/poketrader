ddd45d8a9d4b7a3258cc70becfe7c8f05b82ac1f
paulo vieira <paulorsv01@outlook.com>
2020-11-12 17:18:27 -0300

    Add MeuAnuncioViewController

---
+++ Poketrader/Poketrader/DetalheMeuAnuncio/MeuAnuncioViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MeuAnuncioViewController.swift
+//  Poketrader
+//
+//  Created by Paulo Vieira on 12/11/20.
+//
+
+import UIKit
+
+class MeuAnuncioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
