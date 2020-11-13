dd66653ed3ad81eb49da14d164aa11361b44edbc
Gabriel <gabrieldr@id.uff.br>
2020-11-13 15:38:15 -0300

    Conflito main storyboard & viewController

---
+++ Poketrader/Poketrader/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Poketrader
+//
+//  Created by Paulo Vieira on 01/11/20.
+//
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
