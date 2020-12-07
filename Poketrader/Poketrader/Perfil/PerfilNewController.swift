b4b944815617dacb2075be2320f513f557eea753
paulo vieira <paulorsv01@outlook.com>
2020-12-07 18:07:57 -0300

    Ver dados do usuário e alert para botão de salvar

--- Poketrader/Poketrader/Perfil/PerfilNewController.swift
+++ Poketrader/Poketrader/Perfil/PerfilNewController.swift
@@ -11,6 +11,7 @@ class PerfilNewController: UIViewController {
     
     // Imagens
     @IBOutlet weak var iconUser: UIImageView!
+    private var usuario: Usuario?
     
     // Label
     @IBOutlet weak var fullNameLabel: UILabel!
@@ -25,67 +26,86 @@ class PerfilNewController: UIViewController {
     @IBOutlet weak var telephoneTextField: UITextField!
     
     // to store the current active textfield
-      var activeTextField : UITextField? = nil
+    var activeTextField : UITextField? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Looks for single or multiple taps.
+        self.setupView()
         
-         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        let service: LoginService = LoginService()
+        self.usuario = service.loadUsuario()
+        
+        if let usuario = self.usuario {
+            self.fullNameTextField.text = usuario.nome
+            self.emailTextField.text = usuario.email
+            self.telephoneTextField.text = String(usuario.telefone)
+        }
+        
+    }
+    
+    private func setupView() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
         
         fullNameTextField.delegate = self
         emailTextField.delegate = self
         consoleTextField.delegate = self
         telephoneTextField.delegate = self
-
-        view.addGestureRecognizer(tap)
     }
-
+    
+    
+    @IBAction func pressedSalvarButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Sucesso", message: "Dados salvos com sucesso.", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     //Calls this function when the tap is recognized.
     @objc func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
     
+    
 }
 
 extension PerfilNewController : UITextFieldDelegate {
-  // when user select a textfield, this method will be called
-  func textFieldDidBeginEditing(_ textField: UITextField) {
-    // set the activeTextField to the selected textfield
-    self.activeTextField = textField
-  }
+    // when user select a textfield, this method will be called
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // set the activeTextField to the selected textfield
+        self.activeTextField = textField
+    }
     
-  // when user click 'done' or dismiss the keyboard
-  func textFieldDidEndEditing(_ textField: UITextField) {
-    self.activeTextField = nil
-  }
+    // when user click 'done' or dismiss the keyboard
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.activeTextField = nil
+    }
     @objc func keyboardWillShow(notification: NSNotification) {
-
-      guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
-
-        // if keyboard size is not available for some reason, dont do anything
-        return
-      }
-
-      var shouldMoveViewUp = false
-
-      // if active text field is not nil
-      if let activeTextField = activeTextField {
-
-        let bottomOfTextField = activeTextField.convert(activeTextField.bounds, to: self.view).maxY;
         
-        let topOfKeyboard = self.view.frame.height - keyboardSize.height
-
-        // if the bottom of Textfield is below the top of keyboard, move up
-        if bottomOfTextField > topOfKeyboard {
-          shouldMoveViewUp = true
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            
+            // if keyboard size is not available for some reason, dont do anything
+            return
         }
-      }
-
-      if(shouldMoveViewUp) {
-        self.view.frame.origin.y = 0 - keyboardSize.height
-      }
+        
+        var shouldMoveViewUp = false
+        
+        // if active text field is not nil
+        if let activeTextField = activeTextField {
+            
+            let bottomOfTextField = activeTextField.convert(activeTextField.bounds, to: self.view).maxY;
+            
+            let topOfKeyboard = self.view.frame.height - keyboardSize.height
+            
+            // if the bottom of Textfield is below the top of keyboard, move up
+            if bottomOfTextField > topOfKeyboard {
+                shouldMoveViewUp = true
+            }
+        }
+        
+        if(shouldMoveViewUp) {
+            self.view.frame.origin.y = 0 - keyboardSize.height
+        }
     }
 }
 
