5e1354f30ced2c2544535c27ba56335406778833
paulo vieira <paulorsv01@outlook.com>
2020-12-10 15:24:47 -0300

    Implementei o Alamofire

--- Poketrader/Poketrader/Network/GenericWork.swift
+++ Poketrader/Poketrader/Network/GenericWork.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+enum Method:String{
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
 class GenericWorker {
     
     typealias completion <T> = (_ result: T, _ failure: String?) -> Void
--- Poketrader/Poketrader/Network/NetworkService.swift
+++ Poketrader/Poketrader/Network/NetworkService.swift
@@ -6,12 +6,38 @@
 //
 
 import Foundation
+import Alamofire
 
-class NetworkService {
-    typealias completion <T> = (_ result: T, _ failure: String?) -> Void
+class NetworkService: GenericWorker {
     let urlString: String = "https://pokeapi.co/api/v2/pokemon/"
     
     func getPokemon(nome: String, completion: @escaping completion<Pokemon?>) {
+        let url: URL? = URL(string: urlString + nomes)
+        if let url = url {
+            var request = URLRequest(url: url)
+            request.httpMethod = Method.get.rawValue
+            
+            AF.request(request).responseJSON { (response) in
+
+                if response.response?.statusCode == 200 {
+                    do {
+                        let pokemonData = try JSONDecoder().decode(PokeData.self, from: response.data ?? Data())
+                        let pokemon = Pokemon(data: pokemonData)
+                        completion(pokemon, nil)
+                    } catch {
+                        // deu ruim no parse
+                        completion(nil, "deu ruim")
+                    }
+                } else {
+                    // deu ruim no request
+                    completion(nil, "deu ruim")
+                }
+            }
+            
+        }
+    }
+    
+    func getPokemonURLSession(nome: String, completion: @escaping completion<Pokemon?>) {
         let session: URLSession = URLSession.init(configuration: .default)
         let url: URL? = URL(string: urlString + nome)
         if let url = url {
