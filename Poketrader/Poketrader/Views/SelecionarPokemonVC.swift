2bb5a331f165f3b2e9439d65fa14c99cef55523e
Gabriel <gabrieldr@id.uff.br>
2021-01-19 23:25:39 -0300

    SelecionarPokemon

--- Poketrader/Poketrader/Views/SelecionarPokemonVC.swift
+++ Poketrader/Poketrader/Views/SelecionarPokemonVC.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class SelecionarPokemonVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tableView.register(UINib(nibName: "OfertaCell", bundle: nil), forCellReuseIdentifier: "OfertaCell")
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
     
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension SelecionarPokemonVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
