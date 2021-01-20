a2b2c63fc02ee5b93ef140b4dd8afe1d5c06495c
paulo vieira <paulorsv01@outlook.com>
2021-01-20 14:55:24 -0300

    Adicionei o SDK do Firebase no projeto

--- Poketrader/Poketrader/AppDelegate.swift
+++ Poketrader/Poketrader/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 //@main
 @UIApplicationMain // -- XCode 11.7
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
